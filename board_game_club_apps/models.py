c428647eeaba7398e975513bcd541807af4cb8b3
Uras Ayanoglu <uras@ayanoglu.org>
2022-12-12 13:59:01 +0200

    Create Game Model

--- board_game_club_apps/models.py
+++ board_game_club_apps/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    """Representation of a board game."""
+
+    name = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    year_published = models.IntegerField()
+    publisher = models.CharField(max_length=200)
+    description = models.CharField(max_length=600)
+    number_of_players = models.IntegerField()
+    min_age = models.IntegerField()
+    difficulty = models.CharField(max_length=100)    
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
+
+
