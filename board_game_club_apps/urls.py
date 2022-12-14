20100f50afdb8f79cc2e6e561059629ab56d13f3
Uras Ayanoglu <uras@ayanoglu.org>
2022-12-14 13:49:41 +0200

    Create loans.html, Modify urls.py and views.py

--- board_game_club_apps/urls.py
+++ board_game_club_apps/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = 'board_game_club_apps'
 urlpatterns = [
     # Home Page
-    path('', views.index, name='index'), 
+    path('', views.index, name='index'),
+    # Page that shows all the loans
+    path('loans', views.loans, name='loans'), 
 ]
--- board_game_club_apps/views.py
+++ board_game_club_apps/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Loan
 # Create your views here.
 def index(request):
     """The home page for Board Game Club."""
     return render(request, 'board_game_club_apps/index.html')
+
+def loans(request):
+    """Show all board game loans."""
+    loans = Loan.objects.order_by('loan_date')
+    context = {'loans': loans}
+    return render(request, 'board_game_club_apps/loans.html', context)
