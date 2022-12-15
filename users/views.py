3aef4ef6e0bb50a63115f0b8d789986c61f72a40
Uras Ayanoglu <uras@ayanoglu.org>
2022-12-15 10:11:06 +0200

    Add Register Functionality for Users  to Board Game Club

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    """Register a new Board Game Club User"""
+    if request.method != 'POST':
+        # Display an empty form
+        form = UserCreationForm()
+    else:
+        # Process input from data
+        form = UserCreationForm(data = request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in and then redirect to home page.
+            login(request, new_user)
+            return redirect('board_game_club_apps:index')
+    # Display an empty or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
+    
