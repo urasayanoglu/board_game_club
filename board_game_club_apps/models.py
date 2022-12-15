df71824c6e91aeea92cac12b7bbeaa65433e2555
Uras Ayanoglu <uras@ayanoglu.org>
2022-12-15 10:34:54 +0200

    Add Owner Attribute to Model Board Game Club Apps

--- board_game_club_apps/models.py
+++ board_game_club_apps/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Game(models.Model):
@@ -20,6 +20,7 @@ class Game(models.Model):
         ('a', 'Available'),
         )
     status = models.CharField(max_length=1, choices=loan_status, blank=True, default='a')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
